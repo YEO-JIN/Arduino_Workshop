e4da04169e49002acea65b065400cbdf17c2bec7
YEO-JIN <56580219+YEO-JIN@users.noreply.github.com>
2019-10-17 16:43:48 +0900

    Update ReadAnalogVoltage.ino

--- Basic/ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ Basic/ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -1,16 +1,11 @@
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+ Serial.begin(9600);  
+pinMode(3,OUTPUT);
 }
-
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
+  float value = sensorValue * (255.0 / 1023.0);
+  Serial.println(value);
+  analogWrite(3, value);
 }
