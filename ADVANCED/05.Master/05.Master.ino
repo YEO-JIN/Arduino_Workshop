9dc6d30e6c973da637fc4e335f16147004fe119f
ldh51 <56580314+ldh51@users.noreply.github.com>
2019-10-17 17:55:30 +0900

    Update 05.Master.ino

--- ADVANCED/05.Master/05.Master.ino
+++ ADVANCED/05.Master/05.Master.ino
@@ -1,12 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-int val=analogRead(A0);
-val=map(val,0,1023,0,255);
-Serial.println(val);
+  int val = analogRead(A0);
+  val = map(val, 0, 1023, 0, 255);
+  Serial.print(val);
 }
